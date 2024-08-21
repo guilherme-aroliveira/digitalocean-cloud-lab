281d40b4d9176876fc112209cf03c2f77e00a0dc
Guilherme Oliveira <oliveira.g.ar@proton.me>
2024-08-21 10:33:08 -0300

    feat: add bash script for the server

---
+++ cloud-infrastructure/modules/droplets/install.sh
@@ -0,0 +1,33 @@
+#!\bin\bash
+echo "****************************************"
+echo " Setting the Server" 
+echo "****************************************"
+
+echo "Updating system"
+sudo apt-get update && sudo apt-get upgrade
+
+echo "Installing Java 17"
+sudo apt-get install openjdk-17
+
+echo "Installing docker"
+# install docker
+
+echo "Installing Jenkins"
+# Jenkins
+
+echo "Downloading Vagrant"
+# Vagrant
+
+echo "Installing Vagrant"
+# Vagrant
+
+echo "Installing ansible"
+# ansible
+
+# nginx
+
+echo "****************************************"
+echo " Server Setup Complete"
+echo "****************************************"
+
+
